f7f64b30d5b27d639d655b3ee6b9344cf5859cc4
Lichanyeok <cksgur956@naver.com>
2025-04-27 22:50:51 +0900

    sources commit

---
+++ src/main/java/com/practice/cicdserver/HomeController.java
@@ -0,0 +1,12 @@
+package com.practice.cicdserver;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "CI/CD Pipeline Basic Server v1";
+    }
+}
---
+++ src/test/java/com/practice/cicdserver/CicdserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cicdserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
