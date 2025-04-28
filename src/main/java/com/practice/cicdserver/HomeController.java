32f7f35ca26c1c6a8ea29da44af026aff8a762b3
Lichanyeok <cksgur956@naver.com>
2025-04-28 21:56:32 +0900

    action after commit

--- src/main/java/com/practice/cicdserver/HomeController.java
+++ src/main/java/com/practice/cicdserver/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "CI/CD Pipeline Basic Server v2";
+        return "Action 작성 이후 커밋";
     }
 }
