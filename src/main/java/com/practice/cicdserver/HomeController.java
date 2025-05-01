b1c96fe9a34147cf24bf23750cf86182ac46959b
Lichanyeok <cksgur956@naver.com>
2025-05-01 18:48:06 +0900

    ArgoCD run

--- src/main/java/com/practice/cicdserver/HomeController.java
+++ src/main/java/com/practice/cicdserver/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String home(){
-        return "ArgoCD Application runs";
+        return "ArgoCD Application runs !";
     }
 }
